3ccaaf617959a29fa9818016419ae666b5a7d4fe
cuongnc <codondemen1102@gmail.com>
2022-08-22 21:09:38 +0700

    set up Login page UI

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,7 +1,29 @@
+import { Box, Button, Paper, Typography } from '@mui/material';
+import { Container } from '@mui/system';
 import * as React from 'react';
 
-export interface LoginPageProps {}
-
-export default function LoginPage(props: LoginPageProps) {
-  return <h1>Login Page</h1>;
+export default function LoginPage() {
+  const styles = {
+    container: () => ({
+      display: 'flex',
+      flexFlow: 'row norwap',
+      justifyContent: 'center',
+      alignItems: 'center',
+      minHeight: '100vh',
+    }),
+  };
+  return (
+    <Container sx={styles.container}>
+      <Paper elevation={2} sx={{ p: 3 }}>
+        <Typography variant="h5" component="h1">
+          Student Management
+        </Typography>
+        <Box mt={4}>
+          <Button fullWidth variant="contained" color="primary">
+            Fake Login
+          </Button>
+        </Box>
+      </Paper>
+    </Container>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { CssBaseline } from '@mui/material';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -14,6 +15,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
+        <CssBaseline />
         <App />
       </Router>
     </Provider>
